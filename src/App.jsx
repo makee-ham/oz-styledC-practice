276f034cabd04e04882eee2aaaf0df743e0b089a
Makee <ina0312@naver.com>
2025-05-30 14:48:45 +0900

    모든 css 파일을 Styled Components로 바꿈

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,46 @@
-import "./App.scss";
+import styled, { createGlobalStyle } from "styled-components";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import { flexMixin } from "./styles/styles";
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  color: white;
+}
+
+body {
+  background-color: #121212;
+}
+
+`;
+
+const StyledSection = styled.section`
+  ${flexMixin({
+    justify: "center",
+    align: "center",
+    wrap: "wrap",
+    gap: "20px",
+  })}
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
-    <main>
-      <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
+    <>
+      <main>
+        <GlobalStyle />
+        <Header />
+        <StyledSection>
+          {contents.map((el) => (
+            <Content key={el.id} content={el} />
+          ))}
+        </StyledSection>
+      </main>
+    </>
   );
 }
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,37 @@
+import styled from "styled-components";
+import { flexMixin, fontMixin, gray, tagColor } from "../styles/styles";
+
+const StyledContent = styled.div`
+  ${flexMixin({ direction: "column", align: "flex-start", gap: "5px" })}
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+  span {
+    ${fontMixin({ size: "12px" })}
+    color: ${tagColor};
+    border: 1px solid ${tagColor};
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    ${fontMixin({ size: "18px", weight: "600" })}
+  }
+  p {
+    ${fontMixin({ size: "12px" })}
+    color: ${gray};
+  }
+`;
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+    <StyledContent>
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </StyledContent>
   );
 }
